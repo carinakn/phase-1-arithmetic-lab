d850e018873a9a5382e45816fb5715423c8f63d1
carinakn <carinaknics@gmail.com>
2023-11-09 16:58:37 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.ceil(Math.random()) * 100;
+console.log(random);
+
+let num3 = 20;
+let num4 = 8;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(10, 5, 20, 15, 8);
+console.log(max);
